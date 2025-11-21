f5a6c6dac92f3dd169527327cefe63f32e09ff27
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2025-11-20 23:59:38 -0500

    feat: renderizado de todos

--- src/todos/use-cases/create-todo-html.js
+++ src/todos/use-cases/create-todo-html.js
@@ -1,7 +1,16 @@
 export const createTodoHtml = (todo) => {
     if(!todo) throw new Error('A todo is required');
-    const html = `<h1>${todo.description}</h1>`;
+    const html = `
+        <div class="view">
+            <input class="toggle" type="checkbox" ${todo.done ? 'checked' : ''} />
+            <label>${todo.description}</label>
+            <button class="destroy"></button>
+        </div>
+        <input class="edit" value="Create a TodoMVC template">
+    `;
     const liElement = document.createElement('li');
+    liElement.setAttribute('data-id', todo.id);
+    if(todo.done) liElement.classList.add('completed');
     liElement.innerHTML = html;
     return liElement;
 }
--- src/todos/use-cases/render-todos.js
+++ src/todos/use-cases/render-todos.js
@@ -1,8 +1,12 @@
 import { createTodoHtml } from "./create-todo-html";
 
+let element;
+
 export const renderTodos = (elementID, todos = []) =>{
-    let html = document.getElementById(elementID);
+    if(!element) element = document.getElementById(elementID);
+    if(!element) throw new Error(`Element ${elementID} not found`);
+    element.innerHTML = '';
     todos.forEach(x => {
-        html.append(createTodoHtml(x));
+        element.append(createTodoHtml(x));
     });
 }
