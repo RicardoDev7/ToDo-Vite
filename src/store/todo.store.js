b29c7b603241a8851dfae36b59f91b3f5331d086
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2025-11-20 22:57:07 -0500

    feat: mapeo de metodos

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -19,6 +19,34 @@ const initStore = () => {
     console.log('Init Store');
 }
 
+const loadStore = () => {
+    throw new Error('Not Implemented');
+}
+
+const addTodo = (description) => {
+    throw new Error('Not Implemented');
+}
+
+const toogleTodo = (todoID) => {
+    throw new Error('Not Implemented');
+}
+
+const deleteTodo = (todoID) => {
+    throw new Error('Not Implemented');
+}
+
+const deleteCompleted = () => {
+    throw new Error('Not Implemented');
+}
+
+const setFilter = (newFilter = Filters.All) => {
+    throw new Error('Not Implemented');
+}
+
+const getCurrentFilter = () => {
+    throw new Error('Not Implemented');
+}
+
 export default {
-    initStore,
+    initStore, addTodo, toogleTodo, deleteTodo, deleteCompleted, setFilter, getCurrentFilter
 }
